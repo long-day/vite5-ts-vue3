8070b006a68da4d7aabea8e6e4ad45bb1ca403af
longday <liu.hengjun@qq.com>
2024-06-09 18:50:54 +0800

    2024年6月9日

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
 defineProps<{ msg: string }>();
 
@@ -7,34 +7,39 @@ const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <a-button type="primary">测试AntDesign的按钮</a-button>
-  <div class="card">
-    <button class="bg-red-2" type="button" @click="count++"
-      >count is {{ count }}</button
-    >
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+    <h1>{{ msg }}</h1>
+    <a-button type="primary">测试AntDesign的按钮</a-button>
+    <div class="card">
+        <button class="bg-red-2" type="button" @click="count++">
+            count is {{ count }}
+        </button>
+        <p>
+            Edit
+            <code>components/HelloWorld.vue</code> to test HMR
+        </p>
+    </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <p>
+        Check out
+        <a
+            href="https://vuejs.org/guide/quick-start.html#local"
+            target="_blank"
+        >
+            create-vue
+        </a>
+        ,the official Vue + Vite starter
+    </p>
+    <p>
+        Install
+        <a href="https://github.com/vuejs/language-tools" target="_blank">
+            Volar </a
+        >in your IDE for a better DX
+    </p>
+    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style lang="less" scoped>
 .read-the-docs {
-  color: @test-color;
+    color: @test-color;
 }
 </style>
